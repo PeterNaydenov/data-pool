54e2059b63faed10d556055b67b5d8e9f0fcaa0d
Peter Naydenov <peter.naydenov@gmail.com>
2025-06-21 18:26:27 +0300

    refactoring: setData method

--- src/setData.js
+++ src/setData.js
@@ -1,16 +1,36 @@
 function setData ( dependencies ) {
-    const { db, walk, eBus, readKey, ttlRequest, timeouts } = dependencies;
-    return function setData ( store, k, data ) {
-        const { key, location } = readKey (k);
-        if ( !db[store] )   db[store] = {}
+    const { 
+            db
+            , walk
+            , eBus
+            , readKey
+            , ttlRequest
+            , timeouts
+            , signalNest
+            , signalStores 
+            , validationStore
+        } = dependencies;
+
+    return function setData ( [k, store='default'], data, vFn=false ) {
+        // TODO: Add argument validation
+        const 
+              { key, location } = readKey (k)
+            , hasValidation = vFn instanceof Function
+            , ID = `${store}/${key}`
+            ;
+        if ( !db[store]    )   db[store] = {}
+        if ( hasValidation )   validationStore[ ID ] = vFn
         
         eBus.emit ( store, location, walk({data:db[store][location]}), walk({data})   )
-        db[store][location] = walk ({data})
-        const ttl = ttlRequest[`${store}/${key}`];
+        if      ( signalStores.includes ( store ) && !db[store].hasOwnProperty ( location ))    db[store][location] = signalNest.state ( walk({data}) )
+        else if ( signalStores.includes ( store ) &&  db[store].hasOwnProperty ( location ))    db[store][location].set ( walk({data}) ) 
+        else                                                                                    db[store][location] = walk ({data})
+        
+        const ttl = ttlRequest[ ID ];
         if ( ttl ) {  
                     const timeoutID = timeouts[`${store}/${location}`];
                     if ( timeoutID )   clearTimeout ( timeoutID )
-                    timeouts[`${store}/${location}`] = setTimeout ( () => delete db[store][location], ttl )
+                    timeouts[ ID ] = setTimeout ( () => delete db[store][location], ttl )
             }
 }} // setData func.
 

9de231ee14fed38fed5a3689e1065d3607623f1a
Peter Naydenov <peter.naydenov@gmail.com>
2025-06-24 21:04:32 +0300

    refactoring: Check if store is already defined

--- src/setSignalStore.js
+++ src/setSignalStore.js
@@ -2,7 +2,7 @@ function setSignalStore ( dependencies ) {
     // *** Adds store to signalStores
 
     return ( stores ) => {
-        const { signalStores } = dependencies
+        const { signalStores, db } = dependencies
         // Store or stores names comes as a string, string with list of names separated by comma or array of strings.
         // Before adding we should separate incoming data to array of names
         if ( !stores ) {
@@ -12,9 +12,10 @@ function setSignalStore ( dependencies ) {
             stores = stores.split ( ',' ).map ( s => s.trim () )
            }
         stores.forEach ( s => {
+                        if ( db[s] )  console.error ( `Store "${s}" already exists. ` )  
                         if ( !signalStores.includes ( s ) )   signalStores.push ( s )
             })
-        return stores
+        return signalStores
 }} // setSignalStore func.
 
 
